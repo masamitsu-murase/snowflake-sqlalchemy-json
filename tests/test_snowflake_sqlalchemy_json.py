e1f6b250264562687afa79761f1f5fc82eb7081f
Masamitsu MURASE <masamitsu.murase@gmail.com>
2022-04-25 02:51:59 +0900

    Remove unnecessary code.

--- tests/test_snowflake_sqlalchemy_json.py
+++ tests/test_snowflake_sqlalchemy_json.py
@@ -19,14 +19,6 @@ class Book(Base):
     json_data = Column(JSON)
 
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(255))
-    last_name = Column(String(255))
-    age = Column(Integer)
-
-
 class SnowflakeSqlalchemyJsonTest(unittest.TestCase):
     def compile_statement(self, statement):
         option = {"literal_binds": True}
@@ -40,8 +32,6 @@ class SnowflakeSqlalchemyJsonTest(unittest.TestCase):
         editors = func.flatten(Book.json_data["editors"]).lateral()
         query = select(
             Book.title,
-            Book.json_data["editors"],
-            func.max(Book.json_data["editors"]),
             editors.c.value["name"],
         ).select_from(Book).join(
             editors,
@@ -49,7 +39,7 @@ class SnowflakeSqlalchemyJsonTest(unittest.TestCase):
         ).order_by(editors.c.value["name"].desc())
         sql = self.compile_statement(query)
         expected_sql = """\
-        SELECT prefix1.prefix2.book.title, GET(prefix1.prefix2.book.json_data, 'editors') AS anon_1, max(GET(prefix1.prefix2.book.json_data, 'editors')) AS max_1, GET(anon_3.value, 'name') AS anon_2 
-        FROM prefix1.prefix2.book JOIN LATERAL flatten(INPUT => (GET(prefix1.prefix2.book.json_data, %(json_data_1)s))) AS anon_3 ON GET(anon_3.value, 'type') = 'chief' ORDER BY GET(anon_3.value, 'name') DESC
+        SELECT prefix1.prefix2.book.title, GET(anon_2.value, 'name') AS anon_1 
+        FROM prefix1.prefix2.book JOIN LATERAL flatten(INPUT => (GET(prefix1.prefix2.book.json_data, %(json_data_1)s))) AS anon_2 ON GET(anon_2.value, 'type') = 'chief' ORDER BY GET(anon_2.value, 'name') DESC
         """
         self.assertEqual(sql, textwrap.dedent(expected_sql).rstrip())
